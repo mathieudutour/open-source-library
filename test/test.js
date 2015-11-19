8560db722e01903cc44504adc16ea51f3bb164b5
Mathieu Dutour <mathieu@dutour.me>
2015-11-19 16:24:04 +0000

    Update docs

---
+++ test/test.js
@@ -0,0 +1,15 @@
+import test from 'ava';
+
+test('foo', t => {
+    t.pass();
+    t.end();
+});
+
+test('bar', t => {
+    t.plan(2);
+
+    setTimeout(() => {
+        t.is('bar', 'bar');
+        t.same(['a', 'b'], ['a', 'b']);
+    }, 100);
+});
